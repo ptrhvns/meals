2673148de36ba1280dd62db3d092de33469a2822
Peter Havens <peter.havens@gmail.com>
2022-11-11 10:24:03 -0700

    Add and setup eslint-plugin-react-hooks package

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -6,8 +6,12 @@ module.exports = {
     "plugin:react/jsx-runtime",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   root: true,
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
   settings: {
     react: {
       version: "detect",

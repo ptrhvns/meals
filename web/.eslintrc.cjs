6233e64ab8d9c34d6c387cbe0f452e0d0a78371b
Peter Havens <peter.havens@gmail.com>
2022-11-11 10:19:37 -0700

    Add and setup eslint-plugin-react package

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,6 +1,16 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react"],
   root: true,
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };

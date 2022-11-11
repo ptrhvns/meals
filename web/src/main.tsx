f6a3079870edc6dd6c4d9dc6cd368cd3e751bef9
Peter Havens <peter.havens@gmail.com>
2022-11-11 10:57:19 -0700

    Fix default import linting on TypeScript files

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
 import App from "./routes/App";
 import ErrorPage from "./components/ErrorPage";
 import Home from "./routes/Home";
 import NotFound from "./routes/NotFound";
+import React from "react";
 import Signup from "./routes/Signup";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";

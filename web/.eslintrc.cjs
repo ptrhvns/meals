70fc40b96285af78e289f8fb91ad80faf3822e7c
Peter Havens <peter.havens@gmail.com>
2022-11-11 10:41:51 -0700

    Add and setup eslint import plugin packages

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -5,20 +5,26 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint",
-    "react",
-    "react-hooks",
-    "jsx-a11y",
-  ],
+  plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y", "import"],
   root: true,
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+    "import/no-unresolved": "error",
   },
   settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
     react: {
       version: "detect",
     },

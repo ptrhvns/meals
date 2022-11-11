dbe3a6128ac0d125fe16594ebf947833d00c1f40
Peter Havens <peter.havens@gmail.com>
2022-11-11 10:26:34 -0700

    Add and setup eslint-plugin-jsx-a11y package

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -4,9 +4,15 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+  ],
   root: true,
   rules: {
     "react-hooks/rules-of-hooks": "error",

c5ab6688668efbaf6929e427323398f3f8185cd0
Peter Havens <peter.havens@gmail.com>
2023-01-17 13:46:27 -0700

    Display tags for a recipe

--- web/src/components/Tags.tsx
+++ web/src/components/Tags.tsx
@@ -1,5 +1,8 @@
 import Anchor from "./Anchor";
+import classes from "../styles/components/Tags.module.scss";
+import Paragraph from "./Paragraph";
 import RecipeSectionHeading from "./RecipeSectionHeading";
+import { isEmpty } from "lodash";
 import { RecipeData } from "../lib/types";
 
 interface TagsProps {
@@ -14,6 +17,22 @@ export default function Tags({ recipe }: TagsProps) {
       <RecipeSectionHeading heading="Tags">
         <Anchor to={`/recipe/${recipe.id}/tag/new`}>Create</Anchor>
       </RecipeSectionHeading>
+
+      {isEmpty(recipe.tags) && (
+        <Paragraph className={classes.noTagsMessage}>
+          No tags have been created yet.
+        </Paragraph>
+      )}
+
+      {recipe.tags && (
+        <div className={classes.tags}>
+          {recipe.tags.map((tag) => (
+            <span className={classes.tag} key={tag.id}>
+              {tag.name}
+            </span>
+          ))}
+        </div>
+      )}
     </>
   );
 }

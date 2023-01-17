8acb90178234f10350483a65f10e1c78c1c1b69a
Peter Havens <peter.havens@gmail.com>
2023-01-17 14:21:36 -0700

    Improve accessibility of input errors

--- web/src/components/InputError.tsx
+++ web/src/components/InputError.tsx
@@ -1,4 +1,7 @@
+import * as AccessibleIcon from "@radix-ui/react-accessible-icon";
 import classes from "../styles/components/InputError.module.scss";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface InputErrorProps {
   error?: string;
@@ -6,7 +9,15 @@ interface InputErrorProps {
 
 const InputError = ({ error }: InputErrorProps) => {
   if (!error) return null;
-  return <div className={classes.wrapper}>{error}</div>;
+
+  return (
+    <div className={classes.wrapper}>
+      <AccessibleIcon.Root label="Error">
+        <FontAwesomeIcon icon={faCircleExclamation} />
+      </AccessibleIcon.Root>{" "}
+      {error}
+    </div>
+  );
 };
 
 export default InputError;

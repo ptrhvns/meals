241f83c9558491f9a83d84397f2e742ab53642be
Peter Havens <peter.havens@gmail.com>
2023-03-29 13:56:12 -0600

    Add logging to logout

--- api/main/views/logout.py
+++ api/main/views/logout.py
@@ -1,3 +1,6 @@
+import logging
+from typing import cast
+
 from django.contrib.auth import logout as auth_logout
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -5,10 +8,19 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from main.lib.responses import no_content_response
+from main.models.user import User
+
+logger = logging.getLogger(__name__)
 
 
 @api_view(http_method_names=["POST"])
 @permission_classes([IsAuthenticated])
 def logout(request: Request) -> Response:
+    logger.info(
+        "logging out username %(username)s",
+        {"username": cast(User, request.user).username},
+    )
+
     auth_logout(request)
+
     return no_content_response()

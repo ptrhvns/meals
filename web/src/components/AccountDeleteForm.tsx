02d5e9f85c6174e034f94e6e2ed6229bb4de1182
Peter Havens <peter.havens@gmail.com>
2023-04-05 07:05:40 -0600

    Use Dialog component for confirmation

--- web/src/components/AccountDeleteForm.tsx
+++ web/src/components/AccountDeleteForm.tsx
@@ -6,9 +6,12 @@ import FormError from "./FormError";
 import InputDiv from "./InputDiv";
 import InputError from "./InputError";
 import LabelDiv from "./LabelDiv";
+import Paragraph from "./Paragraph";
 import useApi from "../hooks/useApi";
 import useAuthn from "../hooks/useAuthn";
+import { Dialog, DialogContent } from "./Dialog";
 import { handleApiError } from "../lib/utils";
+import { isEmpty } from "lodash";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
 import { useState } from "react";
@@ -18,6 +21,8 @@ interface FormData {
 }
 
 export default function AccountDeleteForm() {
+  const [confirming, setConfirming] = useState<boolean>(false);
+  const [data, setData] = useState<FormData>();
   const [error, setError] = useState<string>();
   const [submitting, setSubmitting] = useState<boolean>(false);
   const navigate = useNavigate();
@@ -32,52 +37,85 @@ export default function AccountDeleteForm() {
   } = useForm<FormData>();
 
   return (
-    <form
-      className={classes.form}
-      onSubmit={handleSubmit(async (data: FormData) => {
-        setSubmitting(true);
+    <>
+      <Dialog open={confirming}>
+        <DialogContent onDismiss={() => setConfirming(false)}>
+          <Paragraph>
+            Are you sure you want to delete your account, and permanently delete
+            all of your data?
+          </Paragraph>
 
-        if (
-          !window.confirm(
-            "Are you sure you want to delete your account, and permanently delete all of your data?"
-          )
-        ) {
-          setSubmitting(false);
-          return;
-        }
+          <form
+            onSubmit={async (event) => {
+              event.preventDefault();
 
-        const response = await accountDestroy({ data });
-        setSubmitting(false);
+              if (isEmpty(data)) {
+                setConfirming(false);
+                return;
+              }
 
-        if (response.isError) {
-          return handleApiError<FormData>(response, {
-            setError,
-            setFieldError,
-          });
-        }
+              setSubmitting(true);
+              const response = await accountDestroy({ data });
+              setSubmitting(false);
 
-        logout(() => navigate("/", { replace: true }));
-      })}
-    >
-      <FormError error={error} setError={setError} />
+              if (response.isError) {
+                setConfirming(false);
+                handleApiError<FormData>(response, { setError, setFieldError });
+                return;
+              }
 
-      <Field>
-        <LabelDiv htmlFor="password">Password</LabelDiv>
-        <InputDiv
-          disabled={submitting}
-          error={!!fieldErrors?.password?.message}
-          id="password"
-          type="password"
-          {...register("password", { required: "Password is required." })}
-        />
-        <InputError error={fieldErrors?.password?.message} />
-      </Field>
+              logout(() => navigate("/", { replace: true }));
+            }}
+          >
+            <FormActions>
+              <Button
+                color="red"
+                disabled={submitting}
+                variant="filled"
+                type="submit"
+              >
+                Delete
+              </Button>
 
-      <FormActions>
-        <Button color="red" disabled={submitting} type="submit">
-          Delete account
-        </Button>
-      </FormActions>
-    </form>
+              <Button
+                disabled={submitting}
+                onClick={() => setConfirming(false)}
+                type="button"
+              >
+                Dismiss
+              </Button>
+            </FormActions>
+          </form>
+        </DialogContent>
+      </Dialog>
+
+      <form
+        className={classes.form}
+        onSubmit={handleSubmit(async (data: FormData) => {
+          setData(data);
+          setConfirming(true);
+        })}
+      >
+        <FormError error={error} setError={setError} />
+
+        <Field>
+          <LabelDiv htmlFor="password">Password</LabelDiv>
+          <InputDiv
+            disabled={submitting}
+            error={!!fieldErrors?.password?.message}
+            id="password"
+            type="password"
+            {...register("password", { required: "Password is required." })}
+          />
+          <InputError error={fieldErrors?.password?.message} />
+        </Field>
+
+        <FormActions>
+          <Button color="red" disabled={submitting} type="submit">
+            Delete account
+          </Button>
+        </FormActions>
+      </form>
+    </>
   );
 }

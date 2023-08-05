61fb1027dbf4ae122026ee55b2688e87842155a4
Peter Havens <peter.havens@gmail.com>
2023-08-05 13:32:37 -0600

    Add tests

--- api/main/tests/models/direction_test.py
+++ api/main/tests/models/direction_test.py
@@ -1,3 +1,4 @@
+from main.models.direction import MAX_STR_LENGTH
 from main.tests.factories import DirectionFactory, RecipeFactory, UserFactory
 
 
@@ -7,3 +8,12 @@ class TestDunderStrMethod:
         recipe = RecipeFactory.build(user=user)
         direction = DirectionFactory.build(recipe=recipe)
         assert str(direction) == direction.description
+
+    def test_returns_truncated_string_when_too_long(self) -> None:
+        user = UserFactory.build()
+        recipe = RecipeFactory.build(user=user)
+        direction = DirectionFactory.build(
+            recipe=recipe, description="x" * (MAX_STR_LENGTH + 20)
+        )
+        ellipsis_length = 3
+        assert len(str(direction)) == MAX_STR_LENGTH + ellipsis_length
